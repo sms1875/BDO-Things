a017e8f90ba9993cd88573762da860e5b31414a3
sms1875 <01audtjr@gmail.com>
2023-04-17 00:44:44 +0900

    cors-anywhere을 통해 웹 빌드 <-> 프록시 <-> 마켓 통신 heroku계정 오류로 임시주소 사용 임시주소 사용시 인증해야 사용가능-> 웹 배포 사용 안됨 후에 교체해야됨
    https://cors-anywhere.herokuapp.com/

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
-import firebase_core
 import path_provider_foundation
 import screen_retriever
 import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
